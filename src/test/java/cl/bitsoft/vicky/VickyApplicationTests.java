6424cbe6e4099ffa0f15f29ae668d77514cd44e8
Carlos Tapia Molina <ctapia@transbank.cl>
2020-12-06 10:07:11 -0300

    init commit

---
+++ src/test/java/cl/bitsoft/vicky/VickyApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bitsoft.vicky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VickyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
