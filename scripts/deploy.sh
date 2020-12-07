0995b989a0b42152964bb2587d2b95d4074b9762
ctm002 <carlostapiam@gmail.com>
2020-12-07 18:13:54 -0300

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "$REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR" && \
-scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky vicky-0.0.1-SNAPSHOT.jar ctapia@161.35.122.230:/home/ctapia/backend/
+cd $TRAVIS_BUILD_DIR && \
+pwd && \
+ls -la && \
+JAR=$(find $TRAVIS_BUILD_DIR/ -type f -name '*.jar' | sort -n | tail -1) && \
+scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky /home/travis/build/ctm002/vicky/target/vicky-0.0.1-SNAPSHOT.jar ctapia@161.35.122.230:/home/ctapia/backend/
