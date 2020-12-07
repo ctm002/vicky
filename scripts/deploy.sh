00d9404cab96c49ad1f207bc7239983b2fe950fe
ctm002 <carlostapiam@gmail.com>
2020-12-07 11:20:15 -0300

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR" && \
-scp -i ../vicky vicky-0.0.1-SNAPSHOT.jar $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR
+scp -i ../vicky vicky-0.0.1-SNAPSHOT.jar ctapia@161.35.122.230:/home/ctapia/backend/
