9226a0b078cd8df100d3c450392307e34386686c
ctm002 <carlostapiam@gmail.com>
2020-12-07 09:50:25 -0300

    config

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-scp vicky-0.0.1-SNAPSHOT.jar $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && 
---
+++ scripts/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp vicky-0.0.1-SNAPSHOT.jar $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR
