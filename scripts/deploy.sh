a7f9a7662ee89938b2260928a3d4928eb5f5df7a
ctm002 <carlostapiam@gmail.com>
2020-12-07 16:41:10 -0300

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR" && \
-scp -i ../vicky vicky-0.0.1-SNAPSHOT.jar -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ctapia@161.35.122.230:/home/ctapia/backend/
+scp -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ../vicky vicky-0.0.1-SNAPSHOT.jar  ctapia@161.35.122.230:/home/ctapia/backend/
