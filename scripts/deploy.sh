f6945ddfd065d4ec96c74c5a0eb170a25cfa78f2
ctm002 <carlostapiam@gmail.com>
2020-12-07 23:58:58 -0300

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ pwd && \
 ls -la && \
 APP_JAR=$(find $TRAVIS_BUILD_DIR/target -type f -name '*.jar' | sort -n | tail -1) && \
 scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky $APP_JAR ctapia@161.35.122.230:/home/ctapia/backend/ && \
-scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky init.sh ctapia@161.35.122.230:/home/ctapia/backend/
+scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky ./scripts/init.sh ctapia@161.35.122.230:/home/ctapia/backend/
