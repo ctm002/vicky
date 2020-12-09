7670c7f10c6afddb5a7406f3f7f207d4a8450f6b
ctm002 <carlostapiam@gmail.com>
2020-12-09 13:10:20 -0300

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ echo ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_APP_DIR} && \
 cd $TRAVIS_BUILD_DIR && \
 pwd && \
 ls -la && \
-chmod au+rx ./scripts/init.sh && \
+chmod ao+rx ./scripts/init.sh && \
 APP_JAR=$(find $TRAVIS_BUILD_DIR/target -type f -name '*.jar' | sort -n | tail -1) && \
 scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky $APP_JAR ctapia@161.35.122.230:/home/ctapia/backend/ && \
 scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky ./scripts/init.sh ctapia@161.35.122.230:/home/ctapia/backend/
