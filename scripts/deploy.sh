92cda215670c9185c26f8e09b171f3e7a73d1698
ctm002 <carlostapiam@gmail.com>
2020-12-11 10:28:00 -0300

    config logback

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ pwd && \
 ls -la && \
 chmod ao+rx ./scripts/init.sh && \
 APP_JAR=$(find $TRAVIS_BUILD_DIR/target -type f -name '*.jar' | sort -n | tail -1) && \
-scp -r -o StrictHostKeyChecking=no -i ~/.ssh/vicky $APP_JAR ctapia@161.35.122.230:/home/ctapia/backend/ && \
-scp -r -o StrictHostKeyChecking=no -i ~/.ssh/vicky ./scripts/init.sh ctapia@161.35.122.230:/home/ctapia/backend/ && \
+scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky $APP_JAR ctapia@161.35.122.230:/home/ctapia/backend/ && \
+scp -o StrictHostKeyChecking=no -i ~/.ssh/vicky ./scripts/init.sh ctapia@161.35.122.230:/home/ctapia/backend/ && \
 echo "fin scp"
