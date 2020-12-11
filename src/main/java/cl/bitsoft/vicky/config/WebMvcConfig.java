0e8bd2e3679e3266f9d485c3d886b8013d00f9ad
ctm002 <carlostapiam@gmail.com>
2020-12-10 21:54:54 -0300

    controller donativo

--- src/main/java/cl/bitsoft/vicky/config/WebMvcConfig.java
+++ src/main/java/cl/bitsoft/vicky/config/WebMvcConfig.java
@@ -3,9 +3,9 @@ package cl.bitsoft.vicky.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//public class WebMvcConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
-//    }
-//}
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
+    }
+}
