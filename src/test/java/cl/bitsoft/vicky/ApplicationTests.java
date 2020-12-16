96e1f667b128bd6c562b80f8dd64d378ec8d4d67
ctm002 <carlostapiam@gmail.com>
2020-12-16 15:40:15 -0300

    config class file

--- src/test/java/cl/bitsoft/vicky/ApplicationTests.java
+++ src/test/java/cl/bitsoft/vicky/ApplicationTests.java
@@ -1,11 +1,16 @@
 package cl.bitsoft.vicky;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @TestPropertySource(locations = "classpath:test.properties")
+@ActiveProfiles("dev")
 class ApplicationTests {
 
 	@Test
